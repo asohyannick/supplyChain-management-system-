6f7630e80c26e00ddd114c34c02af3b9f3d0fed9
asohyannick <asohyannick3@gmail.com>
2025-07-15 03:20:16 -0700

    Implement security configuration, database setup, and Swagger UI integration

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express, { Application } from 'express';
 import morgan from 'morgan';
 import databaseConfiguration from './config/databaseConfig/databaseConfig';
-import { setupSwagger } from './config/swaggerUI/swaggerUi';
+import { setupSwagger } from './config/swaggerUI/swaggerUI';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
